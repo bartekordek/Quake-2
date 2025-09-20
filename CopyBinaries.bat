7fc1311fe806716620c216a2793c6c11730f2491
Bartłomiej Kordek <bartekordek@gmail.com>
2025-09-20 22:39:45 +0200

    Build fixes.

--- CopyBinaries.bat
+++ CopyBinaries.bat
@@ -8,6 +8,6 @@ rem Assumption 01: Your Quake 2 Data is inside of quake_data dir.
 
 echo Copying file for %CONFIGURATION%
 
-xcopy Build-Windows\%CONFIGURATION%\*.dll %TARGET_DIR% /Y
-xcopy Build-Windows\%CONFIGURATION%\*.exe %TARGET_DIR% /Y
-xcopy Build-Windows\%CONFIGURATION%\*.pdb %TARGET_DIR% /Y
+xcopy Build-Windows\%CONFIGURATION%\*.dll %TARGET_DIR%\ /Y
+xcopy Build-Windows\%CONFIGURATION%\*.exe %TARGET_DIR%\ /Y
+xcopy Build-Windows\%CONFIGURATION%\*.pdb %TARGET_DIR%\ /Y
