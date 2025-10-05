6de793a32047fe46fc2aeda46a1d9a481a471221
Bartłomiej Kordek <bartekordek@gmail.com>
2025-10-05 18:52:38 +0200

    Add stub for game-shared.

---
+++ game-shared/inc/game-shared/config.h
@@ -0,0 +1,10 @@
+#ifndef __game_shared_config_h__
+#define __game_shared_config_h__
+
+#ifdef __cplusplus
+#define EXTERNC extern "C"
+#else
+#define EXTERNC
+#endif
+
+#endif	// __game_shared_config_h__
---
+++ game-shared/inc/game-shared/physics.h
@@ -0,0 +1,8 @@
+#ifndef __game_shared_physics_h__
+#define __game_shared_physics_h__
+
+#include "game-shared/config.h"
+
+void ClipGibVelocity(edict_t *ent);
+
+#endif
