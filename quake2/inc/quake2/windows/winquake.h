01ef0ae0ba6721651a0f519a4cb3de40992a31bf
Bartłomiej Kordek <bartekordek@gmail.com>
2025-11-19 19:34:48 +0100

    Unused windows.h

--- quake2/inc/quake2/windows/winquake.h
+++ quake2/inc/quake2/windows/winquake.h
@@ -21,7 +21,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #pragma warning( disable : 4229 )  // mgraph gets this
 
-#include <windows.h>
 
 #include <dsound.h>
 
