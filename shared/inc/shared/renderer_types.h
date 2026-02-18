72b93a93d075c9ef8c6332e7d7bf85b24db49da7
Bartłomiej Kordek <bartekordek@gmail.com>
2026-02-18 17:40:04 +0100

    Add renderers type enum.

---
+++ shared/inc/shared/renderer_types.h
@@ -0,0 +1,11 @@
+#ifndef __shared_render_type_h__
+#define __shared_render_type_h__
+
+typedef enum 
+{
+	OpenGL_Legacy,
+	OpenGL_Modern,
+	Software
+} RendererTypes;
+
+#endif	// __shared_render_type_h__
